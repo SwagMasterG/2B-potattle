eff66cd02b3e0cdad03841654e0aef6aab058843
Timothy Chiang <mac6@129mac18.valleychristian.net>
2017-02-15 11:02:59 -0800

    made buttons work

--- webapp.js
+++ webapp.js
@@ -6,7 +6,7 @@ var farmer=0;
 var farm=0;
 var factory=0;
 var McDonalds=0;
-var Spaceship=0;
+var spaceship=0;
 
 function potato_clicker(){
 	potato++;
@@ -26,40 +26,52 @@ function shop(sel) {
 			if(potato >= 50 ) {
 				potato-=50;
 				farmer++;
-				update_display();
 			}
 			break;
 		case "farm":
 			if(potato >= 300 ) {
 				potato-=300;
 				farm++;
-				update_display();
 			}
 			break;
 		case "factory":
 			if(potato >= 1500 ) {
 				potato-=1500;
 				farmer++;
-				update_display();
 			}
 			break;
 			case "McDonalds":
 				if(potato >= 5000 ) {
 					potato-=5000;
 					McDonalds++;
-					update_display();
 				}
 				break;
-			case "Spaceship":
+				case "spaceship":
 				if(potato >= 10000 ) {
 					potato-=10000;
-					Spaceship++;
-					update_display();
+					spaceship++;
 				}
-			break;
-						
+				break;
+				
 	}
+	update_display();
 }
 
 // setInterval
-var farmer= setInterval(Function() potato++ 1000)
+setInterval(function() {
+	bonusCalc();
+},1000);
+
+function bonusCalc() {
+	// calculate bonus
+	var bonus = 0;
+	bonus += farmer * 1;
+	bonus += farm * 3;
+	bonus += factory * 5;
+	bonus += McDonalds * 8;
+	bonus += spaceship * 15;
+
+	potato += bonus;
+	update_display();
+
+}
